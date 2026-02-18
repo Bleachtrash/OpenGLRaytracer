ad1f58236f4e7f775b12931b3d2e166c6024c11d
Peter Gloag <petergloag@gmail.com>
2026-02-18 12:41:08 -0800

    Made window unable to be resized

--- include/HelperFuncs.hpp
+++ include/HelperFuncs.hpp
@@ -12,6 +12,7 @@ GLFWwindow* GetWindow(int window_height, int window_width, char *window_name)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
     GLFWwindow *window = glfwCreateWindow(window_width, window_height, window_name, NULL, NULL);
     
     return window;
